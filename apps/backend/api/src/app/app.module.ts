cff3577d3edc512ef77debb61eef2254c042af4a
alex9nines <alex9nines@gmail.com>
2023-12-15 19:47:49 +0300

    adding docker compose, ORM with postgress database

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,10 +1,15 @@
+import { environment } from '../environments/environment';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+        ...environment.connection
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
